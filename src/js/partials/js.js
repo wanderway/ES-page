e3347147d0a00fe280fdb5f594acdf1eeddd7286
Yauheni Humanau <idracat@gmail.com>
2018-10-20 17:06:51 +0300

    customer-block styles without JS and ready register popup

--- src/js/partials/js.js
+++ src/js/partials/js.js
@@ -1,13 +1,32 @@
-// $('.news__items').slick({
-//     settings: 'unslick',
-//     responsive: [
-//         {
-//             breakpoint: 5000,
-//             settings: {
-//                 dots: false,
-//                 arrows: false,
-//                 autoplay: false
-//             }
-//         }
-//     ]
-// });
+var headerRegister = document.querySelector('.header__register-btn');
+var registerPopup = document.querySelector('.register-popup');
+var overlayPopup = document.querySelector('.popup-overlay');
+var registerPopupClose = document.querySelector('.register-popup__close');
+
+headerRegister.addEventListener('click', function () {
+    if (registerPopup.classList.contains('register-popup--hidden')) {
+        registerPopup.classList.remove('register-popup--hidden');
+        registerPopup.classList.add('register-popup--showed');
+    }
+});
+
+headerRegister.addEventListener('click', function () {
+    if (overlayPopup.classList.contains('popup-overlay--hidden')) {
+        overlayPopup.classList.remove('popup-overlay--hidden');
+        overlayPopup.classList.add('popup-overlay--showed');
+    }
+});
+
+registerPopupClose.addEventListener('click', function () {
+    if (registerPopup.classList.contains('register-popup--showed')) {
+        registerPopup.classList.remove('register-popup--showed');
+        registerPopup.classList.add('register-popup--hidden');
+    }
+});
+
+registerPopupClose.addEventListener('click', function () {
+    if (overlayPopup.classList.contains('popup-overlay--showed')) {
+        overlayPopup.classList.remove('popup-overlay--showed');
+        overlayPopup.classList.add('popup-overlay--hidden');
+    }
+});
