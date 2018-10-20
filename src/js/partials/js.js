95c4fa67d8c132981fd8db79ef693b22608befed
Yauheni Humanau <idracat@gmail.com>
2018-10-20 21:14:16 +0300

    full adaptive customer-block + added slider to news/ structure blocks

--- src/js/partials/js.js
+++ src/js/partials/js.js
@@ -30,3 +30,53 @@ registerPopupClose.addEventListener('click', function () {
         overlayPopup.classList.add('popup-overlay--hidden');
     }
 });
+
+$('.news__items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplay: true
+});
+
+$('.customer-block__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+$('.structure-block__items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 10000
+});
+
+
+
+
+
+
