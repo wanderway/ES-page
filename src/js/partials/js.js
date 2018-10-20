116e890fed2685ba2ae92a905c4e0321549f9526
Yauheni Humanau <idracat@gmail.com>
2018-10-20 14:25:14 +0300

    structure-block styles without JS

---
+++ src/js/partials/js.js
@@ -0,0 +1,13 @@
+// $('.news__items').slick({
+//     settings: 'unslick',
+//     responsive: [
+//         {
+//             breakpoint: 5000,
+//             settings: {
+//                 dots: false,
+//                 arrows: false,
+//                 autoplay: false
+//             }
+//         }
+//     ]
+// });
